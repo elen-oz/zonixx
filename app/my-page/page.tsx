d2999ed071417ca3407ad051b95ff09bfbfbc702
elen-oz <ozerova.lenka@gmail.com>
2024-08-22 21:49:08 +0200

    feat: add my-page page

---
+++ app/my-page/page.tsx
@@ -0,0 +1,20 @@
+const MyPage = () => {
+  return (
+    <div className='flex justify-between'>
+      <div>
+        <h2>My mesurments</h2>
+        <a href='https://www.youtube.com/watch?v=15qMh8C1Wzo' target='_blank'>
+          Youtube tutorial: How To Make Beautiful Charts In Next.js
+        </a>
+      </div>
+      <div>
+        <h2>My trainings</h2>
+        <a href='https://www.youtube.com/watch?v=__mSgDEOyv8' target='_blank'>
+          Youtube tutorial PocketBase
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;
