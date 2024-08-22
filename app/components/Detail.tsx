289b5818a1c398dc2135ec7116c1bd7561e3654f
elen-oz <ozerova.lenka@gmail.com>
2024-08-22 21:25:53 +0200

    feat: change layout on detail page

--- app/components/Detail.tsx
+++ app/components/Detail.tsx
@@ -2,11 +2,11 @@ const Detail = ({ exerciseDetail }: any) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
   return (
-    <div className='flex w-full'>
+    <div className='flex w-full justify-center '>
       <div>
         <img src={gifUrl} alt='exercise gif' width={600} />
       </div>
-      <div>
+      <div className='w-[400px]'>
         <h1 className='font-semibold text-xl capitalize'>{name}</h1>
         <p className='text-[#4F4C4C]'>
           Exercises keep you strong.
@@ -14,9 +14,20 @@ const Detail = ({ exerciseDetail }: any) => {
           exercises to target your {target}. It will help you improve your mood
           and gain energy.
         </p>
-        <h3>Body part: {bodyPart}</h3>
-        <h3>Target: {target}</h3>
-        <p>Equipment: {equipment}</p>
+        <div className='w-[200px]'>
+          <div className='grid grid-cols-3 items-center gap-2 my-4'>
+            <img src='/icons/target.svg' alt='target icon' width={30} />
+            <h3 className='col-span-2'>Body part: {bodyPart}</h3>
+          </div>
+          <div className='grid grid-cols-3 items-center gap-2 my-4'>
+            <img src='/icons/hand2.svg' alt='target icon' width={30} />
+            <h3 className='col-span-2'>Muscule: {target}</h3>
+          </div>
+          <div className='grid grid-cols-3 items-center gap-2 my-4'>
+            <img src='/icons/dumbbell.svg' alt='target icon' width={40} />
+            <p className='col-span-2'>Equipment: {equipment}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
