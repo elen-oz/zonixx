2ccd55b545731b840095d04e7b502446ab3e1c0c
elen-oz <ozerova.lenka@gmail.com>
2024-08-29 11:29:04 +0200

    fix: route finaly works!

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,21 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import pool from '../../../lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+import pool from '@/lib/db';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'GET') {
-    const result = await pool.query('SELECT * FROM users');
-    res.status(200).json(result.rows);
-    // } else if (req.method === 'POST') {
-    //   const { name, email } = req.body;
-    //   const result = await pool.query(
-    //     'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    //     [name, email]
-    //   );
-    //   res.status(201).json(result.rows[0]);
-  } else {
-    res.status(405).end(); // Method Not Allowed
+export async function GET() {
+  try {
+    const result = await pool.query('SELECT * FROM exercises');
+    return NextResponse.json(result.rows, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 }
+
+// } else if (req.method === 'POST') {
+//   const { name, email } = req.body;
+//   const result = await pool.query(
+//     'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+//     [name, email]
+//   );
+//   res.status(201).json(result.rows[0]);
