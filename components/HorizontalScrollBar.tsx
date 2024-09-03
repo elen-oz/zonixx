62ebde91cd7dac5fabf22a113999f73f5437e816
elen-oz <ozerova.lenka@gmail.com>
2024-09-03 13:42:07 +0200

    fix: add key in mapping

--- components/HorizontalScrollBar.tsx
+++ components/HorizontalScrollBar.tsx
@@ -4,15 +4,18 @@ const HorizontalScrollBar = ({ data }: any) => {
   return (
     <div className='flex flex-col bg-white m-auto p-auto'>
       <div className='flex overflow-x-scroll pb-10 hide-scroll-bar'>
-        <div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10 '>
+        <ul className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10 '>
           {data.map((item: any) => (
             <div className='inline-block px-3'>
-              <div className='w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out'>
+              <li
+                key={item.id}
+                className='w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out'
+              >
                 <ExerciseCard exercise={item} />
-              </div>
+              </li>
             </div>
           ))}
-        </div>
+        </ul>
       </div>
     </div>
   );
