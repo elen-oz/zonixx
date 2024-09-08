12f5ad8555faf062ba339fb599c994f2258a3389
elen-oz <ozerova.lenka@gmail.com>
2024-09-08 10:01:46 +0200

    test separation

--- components/ExerciseList.tsx
+++ components/ExerciseList.tsx
@@ -1,42 +1,13 @@
-import { Pagination } from '@nextui-org/react';
-
-import ExerciseCard from './ExerciseCard';
+// ExerciseListServer.tsx (Server Component)
 import { ExerciseData } from '@/app/exercises/page';
+import ExerciseListClient from './ExerciseListClient';
 
-type ExerciseListProps = {
+type ExerciseListServerProps = {
   initialExercises: ExerciseData[];
 };
 
-export default async function ExerciseList({
+export default function ExerciseListServer({
   initialExercises,
-}: ExerciseListProps) {
-  const exercisesPerPage = 12;
-  const currentPage = 1;
-  const numberPages = Math.ceil(initialExercises.length / exercisesPerPage);
-
-  const indexOfLastExercise = currentPage * exercisesPerPage;
-  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = initialExercises.slice(
-    indexOfFirstExercise,
-    indexOfLastExercise
-  );
-
-  return (
-    <>
-      <div className='p-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {currentExercises.map((exercise: ExerciseData, index: number) => (
-          <ExerciseCard key={index} exercise={exercise} />
-        ))}
-      </div>
-      <div className='flex justify-center'>
-        <Pagination
-          isCompact
-          showControls
-          total={numberPages}
-          initialPage={currentPage}
-          // onChange={setCurrentPage}
-        />
-      </div>
-    </>
-  );
+}: ExerciseListServerProps) {
+  return <ExerciseListClient initialExercises={initialExercises} />;
 }
---
+++ components/ExerciseListClient.tsx
@@ -0,0 +1,48 @@
+// ExerciseListClient.tsx (Client Component)
+'use client';
+
+import { useState } from 'react';
+import { Pagination } from '@nextui-org/react';
+import ExerciseCard from './ExerciseCard';
+import { ExerciseData } from '@/app/exercises/page';
+
+type ExerciseListClientProps = {
+  initialExercises: ExerciseData[];
+};
+
+export default function ExerciseListClient({
+  initialExercises,
+}: ExerciseListClientProps) {
+  const exercisesPerPage = 12;
+  const [currentPage, setCurrentPage] = useState(1);
+  const numberPages = Math.ceil(initialExercises.length / exercisesPerPage);
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = initialExercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  return (
+    <>
+      <ul className='p-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {currentExercises.map((exercise: ExerciseData, index: number) => (
+          <li key={index}>
+            <ExerciseCard key={index} exercise={exercise} />
+          </li>
+        ))}
+      </ul>
+      <div className='flex justify-center'>
+        <Pagination
+          isCompact
+          showControls
+          total={numberPages}
+          initialPage={1}
+          page={currentPage}
+          onChange={setCurrentPage}
+        />
+      </div>
+    </>
+  );
+}
