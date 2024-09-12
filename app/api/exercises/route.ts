598388c6deffe6023e36216d3446f5ca42cf065a
elen-oz <ozerova.lenka@gmail.com>
2024-09-12 12:12:13 +0200

    feat: add POST req for creating exercise

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,6 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
 
+type responseItemType = {
+  id: string;
+  name: string;
+  body_part: string;
+  equipment: string;
+  gif_url: string;
+  target: string;
+  secondary_muscles: string[];
+  instructions: string[];
+};
+
 export async function GET() {
   try {
     const result = await pool.query('SELECT * FROM exercises');
@@ -13,10 +24,36 @@ export async function GET() {
   }
 }
 
-// } else if (req.method === 'POST') {
-//   const { name, email } = req.body;
-//   const result = await pool.query(
-//     'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-//     [name, email]
-//   );
-//   res.status(201).json(result.rows[0]);
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const {
+      id,
+      name,
+      body_part,
+      equipment,
+      gif_url,
+      target,
+      secondary_muscles,
+      instructions,
+    } = body;
+
+    if (!id || !name || !body_part || !equipment || !gif_url || !target || !secondary_muscles || !instructions) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+    }
+
+    const result = await pool.query(
+        `INSERT INTO exercises (id, name, body_part, equipment, gif_url, target, secondary_muscles, instructions)
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+        [id, name, body_part, equipment, gif_url, target, secondary_muscles, instructions]
+    );
+
+    return NextResponse.json(result.rows[0], { status: 201 });
+  } catch (error) {
+    console.error('Error inserting exercise:', error);
+    return NextResponse.json(
+        { error: 'Internal Server Error' },
+        { status: 500 }
+    );
+  }
+}
