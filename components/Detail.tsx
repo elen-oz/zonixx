e1cfb373239779f1b7864172c19314575355c7dd
elen-oz <ozerova.lenka@gmail.com>
2024-09-14 15:45:38 +0200

    feat: add button

--- components/Detail.tsx
+++ components/Detail.tsx
@@ -1,43 +1,71 @@
 import Image from "next/image";
-import {simplifyExerciseUrl} from "@/lib/utils";
+import { Button } from "@nextui-org/react";
+import { simplifyExerciseUrl } from "@/lib/utils";
 
 const Detail = ({ exerciseDetail }: any) => {
   const { id, bodyPart, gif_url, name, target, equipment } = exerciseDetail;
   const newGifUrl = simplifyExerciseUrl(gif_url);
 
-  console.log('--id: ', id);
-  console.log('--name: ', name);
-  console.log('--gif_url: ', gif_url);
-  console.log('--newGifUrl: ', newGifUrl);
+  console.log("--id: ", id);
+  console.log("--name: ", name);
+  console.log("--gif_url: ", gif_url);
+  console.log("--newGifUrl: ", newGifUrl);
 
   return (
-    <div className='flex flex-col md:flex-row w-full justify-center items-center mb-10'>
+    <div className="flex flex-col md:flex-row w-full justify-center items-center mb-10">
       <div>
-        <Image src={newGifUrl} alt='exercise gif' className='max-w-[400px]' width={400} height={400} />
+        <Image
+          src={newGifUrl}
+          alt="exercise gif"
+          className="max-w-[400px]"
+          width={400}
+          height={400}
+        />
       </div>
-      <div className='max-w-[400px]'>
-        <h1 className='font-semibold text-xl capitalize'>{name}</h1>
-        <div className=''>
-          <div className='grid grid-cols-4 items-center gap-2 my-4'>
-            <Image src='/icons/target.svg' alt='target icon' width={30} height={30} />
-            <h4 className='col-span-3'>
-              Body part:{' '}
-              <span className='font-semibold capitalize'>{bodyPart}</span>
+      <div className="max-w-[400px]">
+        <h1 className="font-semibold text-xl capitalize">{name}</h1>
+        <div className="">
+          <div className="grid grid-cols-4 items-center gap-2 my-4">
+            <Image
+              src="/icons/target.svg"
+              alt="target icon"
+              width={30}
+              height={30}
+            />
+            <h4 className="col-span-3">
+              Body part:{" "}
+              <span className="font-semibold capitalize">{bodyPart}</span>
             </h4>
           </div>
-          <div className='grid grid-cols-4 items-center gap-2 my-4'>
-            <Image src='/icons/hand2.svg' alt='target icon' width={30} height={30} />
-            <h4 className='col-span-3'>
-              Muscule:{' '}
-              <span className='font-semibold capitalize'>{target}</span>
+          <div className="grid grid-cols-4 items-center gap-2 my-4">
+            <Image
+              src="/icons/hand2.svg"
+              alt="target icon"
+              width={30}
+              height={30}
+            />
+            <h4 className="col-span-3">
+              Muscule:{" "}
+              <span className="font-semibold capitalize">{target}</span>
             </h4>
           </div>
-          <div className='grid grid-cols-4 items-center gap-2 my-4'>
-            <Image src='/icons/dumbbell.svg' alt='target icon' width={35} height={35} />
-            <h4 className='col-span-3'>
-              Equipment:{' '}
-              <span className='font-semibold capitalize'>{equipment}</span>
+          <div className="grid grid-cols-4 items-center gap-2 my-4">
+            <Image
+              src="/icons/dumbbell.svg"
+              alt="target icon"
+              width={35}
+              height={35}
+            />
+            <h4 className="col-span-3">
+              Equipment:{" "}
+              <span className="font-semibold capitalize">{equipment}</span>
             </h4>
+          </div>
+
+          <div className="p-4">
+            <Button color="primary" className="px-4">
+              Add to my training
+            </Button>
           </div>
         </div>
       </div>
