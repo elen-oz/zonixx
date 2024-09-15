8a926366f8b59a3a156333ec787fc5dc89da899f
elen-oz <ozerova.lenka@gmail.com>
2024-09-15 10:34:42 +0200

    feat: update profile page view for trainings days

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,21 +2,48 @@ import WorkoutsList from "@/components/WorkoutsList";
 
 const MyPage = () => {
   return (
-    <div className="flex justify-between">
+    <>
       <div>
-        <h2>My mesurments</h2>
-        <a href="https://www.youtube.com/watch?v=15qMh8C1Wzo" target="_blank">
-          Youtube tutorial: How To Make Beautiful Charts In Next.js
-        </a>
-      </div>
-      <div>
-        <h2>My Workouts</h2>
-        {/*<a href='https://www.youtube.com/watch?v=__mSgDEOyv8' target='_blank'>*/}
-        {/*  Youtube tutorial PocketBase*/}
-        {/*</a>*/}
+        <h2>My Favorite Workouts</h2>
         <WorkoutsList />
       </div>
-    </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 border-2 border-primary rounded-lg p-4">
+        <div className="border-2 rounded-lg p-4">
+          <h3 className="text-center">1 workout day</h3>
+          <ul>
+            <li>exercise 1</li>
+            <li>exercise 2</li>
+            <li>exercose 3</li>
+            <li>exercise 4</li>
+          </ul>
+        </div>
+        <div className="border-2 rounded-lg p-4">
+          <h3 className="text-center">2 workout day</h3>
+          <ul>
+            <li>exercise 1</li>
+            <li>exercise 2</li>
+            <li>exercose 3</li>
+            <li>exercise 4</li>
+            <li>exercise 5</li>
+          </ul>
+        </div>
+        <div className="border-2 rounded-lg p-4">
+          <h3 className="text-center">3 workout day</h3>
+          <ul>
+            <li>exercise 1</li>
+            <li>exercise 2</li>
+          </ul>
+        </div>
+
+        {/*<div>*/}
+        {/*  <h2>My mesurments</h2>*/}
+        {/*  <a href="https://www.youtube.com/watch?v=15qMh8C1Wzo" target="_blank">*/}
+        {/*    Youtube tutorial: How To Make Beautiful Charts In Next.js*/}
+        {/*  </a>*/}
+        {/*</div>*/}
+      </div>
+    </>
   );
 };
 
