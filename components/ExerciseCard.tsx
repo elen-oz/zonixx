fff520316cdfe2c87a55742c3b690cfae07dc51c
elen-oz <ozerova.lenka@gmail.com>
2024-09-16 16:00:14 +0200

    feat: add add-btn to card component

--- components/ExerciseCard.tsx
+++ components/ExerciseCard.tsx
@@ -2,63 +2,68 @@
 
 import React from 'react';
 import {
-  Modal,
-  useDisclosure,
-  Chip,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
+    Modal,
+    useDisclosure,
+    Chip,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
     Image
 } from '@nextui-org/react';
-import { IoIosInformationCircleOutline } from 'react-icons/io';
+import {IoIosInformationCircleOutline} from 'react-icons/io';
 import Link from 'next/link';
-import { ExerciseData } from '@/app/exercises/page';
+import {ExerciseData} from '@/app/exercises/page';
 import ModalInfo from './ModalInfo';
+import {AddWorkoutButton} from './AddWorkoutButton';
 
 type ExerciseCardProps = {
-  exercise: ExerciseData;
+    exercise: ExerciseData;
 };
 
-const ExerciseCard = ({ exercise }: ExerciseCardProps) => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+const ExerciseCard = ({exercise}: ExerciseCardProps) => {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
-  // todo: fix gifs display
+    // todo: fix gifs display
 
-  return (
-    <Card className='h-full pb-4 justify-between hover:shadow-xl hover:transition-all ease-in-out hover:border-1'>
-      <CardHeader>
-        <IoIosInformationCircleOutline
-          onClick={onOpen}
-          size={40}
-          className='p-2 cursor-pointer'
-        />
+    return (
+        <Card className='h-full pb-4 justify-between hover:shadow-xl hover:transition-all ease-in-out hover:border-1'>
+            <CardHeader className='flex items-center justify-between'>
+                <IoIosInformationCircleOutline
+                    onClick={onOpen}
+                    size={40}
+                    className='p-2 cursor-pointer'
+                />
 
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top'>
-          <ModalInfo exercise={exercise} />
-        </Modal>
-      </CardHeader>
-      <Link href={`/exercises/${exercise.id}`} className='h-full'>
-        <CardBody className='overflow-visible py-2'>
-            <Image
-                isZoomed
-              alt={exercise.name}
-              className='object-cover rounded-xl h-full'
-              src={exercise.gif_url}
-              width={270}
-              height={270}
-                fallbackSrc="https://via.placeholder.com/300x200"
-            />
-        </CardBody>
-        <CardFooter className='flex-col justify-between'>
-          <h4 className='text-tiny uppercase font-bold pb-2'>
-            {exercise.name}
-          </h4>
-          <Chip className='bg-warning-500 text-white'>{exercise.target}</Chip>
-        </CardFooter>
-      </Link>
-    </Card>
-  );
+                <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top'>
+                    <ModalInfo exercise={exercise}/>
+                </Modal>
+
+                <div className=''>
+                    <AddWorkoutButton exercise={exercise} additionStyle={'border-2 border-primary bg-white text-primary text-sm hover:bg-primary hover:text-white'}>Add</AddWorkoutButton>
+                </div>
+            </CardHeader>
+            <Link href={`/exercises/${exercise.id}`} className='h-full'>
+                <CardBody className='overflow-visible py-2'>
+                <Image
+                        isZoomed
+                        alt={exercise.name}
+                        className='object-cover rounded-xl h-full'
+                        src={exercise.gif_url}
+                        width={270}
+                        height={270}
+                        fallbackSrc="https://via.placeholder.com/270x270"
+                    />
+                </CardBody>
+                <CardFooter className='flex-col justify-between'>
+                    <h4 className='text-tiny uppercase font-bold pb-2'>
+                        {exercise.name}
+                    </h4>
+                    <Chip className='bg-warning-500 text-white'>{exercise.target}</Chip>
+                </CardFooter>
+            </Link>
+        </Card>
+    );
 };
 
 export default ExerciseCard;
