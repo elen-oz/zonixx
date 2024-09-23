0425501bed09d2320e2372d3b60ec2a1fdb5ce03
elen-oz <ozerova.lenka@gmail.com>
2024-09-23 10:52:02 +0200

    fix some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from '@nextui-org/button';
+import {Button} from '@nextui-org/button';
 
 export default function Home() {
-  return (
-    <div className='w-full flex flex-col items-center justify-center py-12'>
-      <div className='text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20'>
-        <h1 className='text-md text-black uppercase font-bold'>
-          Achieve your fitness goals
-        </h1>
-        <h2 className='max-w-[700px] mx-auto '>
-          <span className='block text-8xl tracking-tighter'>find your strength</span>
-          {/*<span className='block font-heading text-primary'>It&#x27;s today or never.</span>*/}
-          <span className='block text-primary text-8xl tracking-tighter'>inside <br/>and out.</span>
-        </h2>
-        <div className='mt-12 inline-flex'>
-          <Button
-              as={Link}
-              href='/exercises'
-            // color='danger'
-            className='font-semibold shadow'
-          >
-            Get started
-          </Button>
+    return (
+        <div className='w-full flex flex-col items-center justify-center py-12'>
+            <div className='text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20'>
+                <h1 className='text-md text-black uppercase'>
+                    Achieve your fitness goals
+                </h1>
+                <h2 className='max-w-[700px] mx-auto '>
+                    <span className='block big-title'>find your strength</span>
+                    {/*<span className='block font-heading text-primary'>It&#x27;s today or never.</span>*/}
+                    <span className='block text-primary big-title'>inside <br/>and out.</span>
+                </h2>
+                <div className='mt-12 inline-flex'>
+                    <Button
+                        as={Link}
+                        href='/exercises'
+                        className='button font-semibold shadow'
+                    >
+                        Get started
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,51 +1,57 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 import Image from "next/image";
 
 const Header = () => {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <header>
-      <nav>
-        <ul className="w-full flex justify-between items-center">
-          <li className='text-3xl font-extrabold text-black sm:text-5xl font-heading uppercase'>zonixx</li>
-          <li
-            className={`${
-              pathname === "/exercises"
-                ? "font-semibold bg-white rounded-t-xl"
-                : ""
-            } pt-0 pb-6 sm:pt-3 sm:pr-8 sm:pl-4`}
-          >
-            <Link
-              href="/exercises"
-              className="flex justify-center items-center gap-2"
-            >
+    return (
+        <header>
+            <nav>
+                <ul className="w-full flex justify-between items-center">
+                    <li>
+                        <Link
+                            href="/"
+                            className='text-3xl font-extrabold text-black sm:text-5xl font-heading uppercase'
+                        >
+                            zonixx
+                        </Link></li>
+                    <li
+                        className={`${
+                            pathname === "/exercises"
+                                ? "active-nav-link"
+                                : ""
+                        } px-2`}
+                    >
+                        <Link
+                            href="/exercises"
+                            className="flex justify-center items-center gap-2"
+                        >
               <span className="hidden sm:inline-block capitalize">
                 exercises
               </span>
-            </Link>
-          </li>
-          <li
-            className={`${
-              pathname === "/profile"
-                ? "font-semibold bg-white rounded-t-xl"
-                : ""
-            } pt-0 pb-6 sm:pt-3 sm:pr-8 sm:pl-4`}
-          >
-            <Link
-              href="/profile"
-              className="flex justify-center items-center gap-2"
-            >
-              <span className="hidden sm:inline-block capitalize">profile</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+                        </Link>
+                    </li>
+                    <li
+                        className={`${
+                            pathname === "/profile"
+                                ? "active-nav-link"
+                                : ""
+                        } px-2`}
+                    >
+                        <Link
+                            href="/profile"
+                            className="flex justify-center items-center gap-2"
+                        >
+                            <span className="hidden sm:inline-block capitalize">profile</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
