f48188d9b8401ef2df7eda97d2175eecc27d85ea
elen-oz <ozerova.lenka@gmail.com>
2024-09-23 11:40:21 +0200

    feat: add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import {Button} from "@nextui-org/react";
+
+export default function NotFound() {
+    return (
+        <div className="h-full md:h-[600px]">
+            <div className="flex h-full flex-col items-center justify-center gap-2">
+                <h2 className="text-6xl font-bold">404 Not Found</h2>
+                <p>The page you are looking for does not exist.</p>
+
+                <p className='text-2xl'>Let&#x27;s start again?</p>
+                <Button radius="none" variant="ghost">
+                    <Link
+                        href="/"
+
+                    >
+                        Yes, please
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    );
+}
