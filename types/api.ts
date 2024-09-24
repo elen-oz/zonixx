2b6c189d299a5abaa61ea73607379cc377369403
elen-oz <ozerova.lenka@gmail.com>
2024-09-24 09:01:33 +0200

    feat: add types - to use later

--- types/api.ts
+++ types/api.ts
@@ -13,6 +13,11 @@ export interface YoutubeVideo {
     };
 }
 
+export type BodyPart = "full body" | "core" | "upper body" | "back" | "cardio" | "chest" | "lower arms" | "lower legs" | "legs" | "neck" | "shoulders" | "upper arms" | "upper legs" | "waist";
+export type Equipment = "assisted" | "band" | "barbell" | "body weight" | "bosu ball" | "cable" | "dumbbell" | "elliptical machine" | "ez barbell" | "hammer" | "kettlebell" | "leverage machine" | "medicine ball" | "olympic barbell" | "resistance band" | "roller" | "rope" | "skierg machine" | "sled machine" | "smith machine" | "stability ball" | "stationary bike" | "stepmill machine" | "tire" | "trap bar" | "upper body ergometer" | "weighted" | "wheel roller";
+export type Target = "abductors" | "abs" | "adductors" | "biceps" | "calves" | "cardiovascular system" | "delts" | "forearms" | "glutes" | "hamstrings" | "lats" | "levator scapulae" | "pectorals" | "quads" | "serratus anterior" | "spine" | "traps" | "triceps" | "upper back";
+
+
 export interface Exercise {
     id: string;
     name: string;
@@ -32,3 +37,4 @@ export interface ApiOptions {
         [key: string]: string;
     };
 }
+
