165389b9c80e72ff2a720852ba343b22b3f17b44
elen-oz <ozerova.lenka@gmail.com>
2024-09-24 10:16:32 +0200

    feat: add functionality to trainings days

--- components/DayExerciseList.tsx
+++ components/DayExerciseList.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import { useExerciseStore } from '@/store/useExerciseStore';
+import {IoIosInformation, IoIosRemove} from "react-icons/io";
+import {useTrainingDaysStore} from '@/store/useTrainingDaysStore';
 import AddExerciseToDayDropdown from "./AddExerciseToDayDropdown";
 import type {TrainingDay} from "@/types/api"
+import React, {useEffect} from "react";
+import {Modal, useDisclosure} from "@nextui-org/react";
+import ModalInfo from "@/components/ModalInfo";
 
 interface DayExerciseListProps {
     day: TrainingDay;
 }
 
-export default function DayExerciseList({ day }: DayExerciseListProps) {
-    const exercises = useExerciseStore((state) => state[day]);
+export default function DayExerciseList({day}: DayExerciseListProps) {
+    const {
+        trainingDay1,
+        trainingDay2,
+        trainingDay3,
+        removeExerciseFromDay,
+        loadExercises
+    } = useTrainingDaysStore();
+    const exercises = useTrainingDaysStore((state) => state[day]);
+    // console.log('-=-=-= exercises', exercises);
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+
+    const handleInfoClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onOpen();
+    };
+
+    const handleRemoveExerciseClick = (e: React.MouseEvent) => {
+        // e.preventDefault();
+        // e.stopPropagation();
+        // removeExerciseFromDay(day, exercise.id);
+    };
+
+
+    // removeExerciseFromDay('day2', 3);
+
+    useEffect(() => {
+        loadExercises();
+    }, []);
 
     return (
         <div className=''>
@@ -17,7 +50,22 @@ export default function DayExerciseList({ day }: DayExerciseListProps) {
 
             <ol className='list-decimal px-4 mb-4'>
                 {exercises.map((exercise) => (
-                    <li key={exercise.id} className='capitalize'>{exercise.name}</li>
+                    <li key={exercise.id} className='flex items-center justify-between capitalize'>
+                        <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top' backdrop='blur'>
+                            <ModalInfo exercise={exercise} isFavorite={true}/>
+                        </Modal>
+                        <div className='flex items-center justify-between gap-2'>
+                            <IoIosInformation
+                                onClick={handleInfoClick}
+                                size={15}
+                                className='cursor-pointer bg-primary-100 hover:bg-primary-50 rounded-full'
+                            />
+                            <span>{exercise.name}</span>
+                        </div>
+
+
+                        <IoIosRemove onClick={()=>removeExerciseFromDay(day, exercise.id)} className='cursor-pointer hover:bg-primary-50 rounded-full'/>
+                    </li>
                 ))}
 
             </ol>
