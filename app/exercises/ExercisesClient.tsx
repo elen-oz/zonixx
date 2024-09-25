897adf1a3fd2c7f5514e47f9be81b2e48e8c82ee
elen-oz <ozerova.lenka@gmail.com>
2024-09-25 12:37:23 +0200

    feat: add start section to exercise page

--- app/exercises/ExercisesClient.tsx
+++ app/exercises/ExercisesClient.tsx
@@ -8,6 +8,7 @@ import { Chip } from '@nextui-org/react';
 import Image from 'next/image';
 import HeroImg from '@/app/assets/images/hero5.webp';
 import { fetchExercises } from '@/lib/api';
+import StartSection from '@/app/exercises/StartSection';
 
 const bodyParts = [
   'all',
@@ -83,37 +84,7 @@ const ExercisesClient = () => {
       {isLoading && <p>Loading exercises...</p>}
       {error && <p className="error">{error}</p>}
 
-      {exercises.length > 0 ? (
-        <ExerciseList exercises={exercises} />
-      ) : (
-        <section className="mb-10">
-          <div className="flex items-center justify-center">
-            <h2 className="text-xl sm:text-3xl md:text-4xl mt-7">
-              push yourself harder to become better
-            </h2>
-          </div>
-
-          <div className="relative">
-            <Image
-              src={HeroImg}
-              alt="image"
-              width={1112}
-              className="w-full h-[300px] md:h-[600px] object-cover rounded-[4rem]"
-            />
-            <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-9">
-              <h2 className="text-white big-title text-xl sm:text-4xl md:text-6xl">
-                <span className="font-heading text-violet-900/95 text-xl sm:text-4xl md:text-6xl">
-                  Start search
-                </span>{' '}
-                by <br />
-                body part, <br />
-                target muscle <br />
-                or exercise name
-              </h2>
-            </div>
-          </div>
-        </section>
-      )}
+      {exercises.length > 0 ? <ExerciseList exercises={exercises} /> : <StartSection />}
     </>
   );
 };
---
+++ app/exercises/StartSection.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import HeroImg from '@/app/assets/images/hero5.webp';
+
+const StartSection = () => (
+  <section className="mb-10">
+    <div className="flex items-center justify-center">
+      <h2 className="text-xl sm:text-3xl md:text-4xl mt-7">
+        push yourself harder to become better
+      </h2>
+    </div>
+
+    <div className="relative">
+      <Image
+        src={HeroImg}
+        alt="image"
+        width={1112}
+        className="w-full h-[300px] md:h-[600px] object-cover rounded-[4rem]"
+      />
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-9">
+        <h2 className="text-white big-title text-xl sm:text-4xl md:text-6xl">
+          <span className="font-heading text-violet-900/95 text-xl sm:text-4xl md:text-6xl">
+            Start search
+          </span>{' '}
+          by <br />
+          body part, <br />
+          target muscle <br />
+          or exercise name
+        </h2>
+      </div>
+    </div>
+  </section>
+);
+
+export default StartSection;
